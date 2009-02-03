dd0a793b12e54d4983f8969ce1b407c88c7b3240
Brian Eng <brian@brianeng.com>
2009-02-03 16:58:02 -0600

    use geokit for smarter addresses

--- lib/mapbot.rb
+++ lib/mapbot.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
-gem 'twitter', '0.4.1'
-gem 'sequel', '2.10.0'
-
 require 'twitter'
+require 'geokit'
 require 'sequel'
+require 'cgi'
+require 'open-uri'
 
-DB = Sequel.sqlite('/Users/eng/code/mapbot/db/mapbot.sqlite3')
+Geokit::Geocoders::timeout = 3
+Geokit::Geocoders::yahoo = 't.5J7cPV34GSc36G4E977U17VGt21xQBYliOgQqBFCZUm0.b8CklRnFB58LVgL8o'
+Geokit::Geocoders::google = 'ABQIAAAAt7tTULp7SNQT-qPTLSMtjRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRm5nERXrpuW78knxyegyHlxQGnvw'
+Geokit::Geocoders::geocoder_us = false 
+Geokit::Geocoders::provider_order = [:google,:us,:yahoo]
+
+DB = Sequel.sqlite('db/mapbot.sqlite3')
 @twitter = Twitter::Base.new('mapbot', 'lem0n!ce')
