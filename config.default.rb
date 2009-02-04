d2ecb0721e9b437f2fa15f12f7c47d8b5044f650
Brian Eng <brian@brianeng.com>
2009-02-04 09:53:10 -0600

    default config file

--- config.default.rb
+++ config.default.rb
@@ -1,13 +1,13 @@
 Mapbot.init do
   Mapbot.db_path = 'db/mapbot.sqlite3'
-  Mapbot.twitter_username = 'mapbot'
-  Mapbot.twitter_password = 'lem0n!ce'
-  Mapbot.yelp_api_key = 'DX0Kb0pB62uyQSuK74nFEA'
+  Mapbot.twitter_username = 'PUT YOURS HERE'
+  Mapbot.twitter_password = 'PUT YOURS HERE'
+  Mapbot.yelp_api_key = 'PUT YOURS HERE'
 end
 
 include Geokit::Geocoders
 Geokit::Geocoders::timeout = 3
-Geokit::Geocoders::yahoo = 't.5J7cPV34GSc36G4E977U17VGt21xQBYliOgQqBFCZUm0.b8CklRnFB58LVgL8o'
-Geokit::Geocoders::google = 'ABQIAAAAt7tTULp7SNQT-qPTLSMtjRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRm5nERXrpuW78knxyegyHlxQGnvw'
+Geokit::Geocoders::yahoo = 'PUT YOURS HERE'
+Geokit::Geocoders::google = 'PUT YOURS HERE'
 Geokit::Geocoders::geocoder_us = false 
 Geokit::Geocoders::provider_order = [:google, :yahoo, :us]
